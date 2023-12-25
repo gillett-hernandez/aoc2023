c6b7da3d2e46a478fd567d373e11a00d2244ed10
Gillett Hernandez <gillett.hernandez@gmail.com>
2023-12-24 18:15:20 -0800

    solve pt 1 of day 10

--- src/bin/day00.rs
+++ src/bin/day00.rs
@@ -1,10 +1,7 @@
-
 fn main() {
     use std::fs::File;
     use std::io::{BufRead, BufReader};
-    let file = File::open("data/day1.txt").unwrap();
+    let file = File::open("samples/dayXX.txt").unwrap();
     let reader = BufReader::new(file);
     let mut lines = reader.lines();
-
-
 }
